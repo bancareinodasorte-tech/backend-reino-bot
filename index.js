d7c40fb64c6f662803c77d76d6841a317d38f42c
bancareinodasorte-tech <bancareinodasorte@gmail.com>
2026-05-05 20:08:18 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
 // Reino Zap V13
 console.log('V13 ativo');
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Reino Zap V13 rodando 🚀');
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log('Servidor rodando na porta ' + PORT);
-});
